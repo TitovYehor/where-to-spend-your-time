d51ac1a3f9ecd0764a61bbd07fdf9718ddd0f0a2
Yehor Titov <tegor901@gmail.com>
2025-06-21 14:36:19 +0300

    - Add Category and Item Dtos

---
+++ WhereToSpendYourTime.Api/Models/Category/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WhereToSpendYourTime.Api.Models.Category;
+
+public class CategoryDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+}
---
+++ WhereToSpendYourTime.Api/Models/Item/ItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WhereToSpendYourTime.Api.Models.Item;
+
+public class ItemDto
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+    
+    public string CategoryName { get; set; } = string.Empty;
+}
