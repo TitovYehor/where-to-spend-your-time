2371f38da5b9e05d06707248377dc194c78b167b
Yehor Titov <tegor901@gmail.com>
2025-06-21 14:37:49 +0300

    - Add CategoriesController - Add ItemsController

---
+++ WhereToSpendYourTime.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhereToSpendYourTime.Api.Models.Category;
+using WhereToSpendYourTime.Api.Models.Item;
+using WhereToSpendYourTime.Data;
+
+namespace WhereToSpendYourTime.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private readonly AppDbContext _db;
+    private readonly IMapper _mapper;
+
+    public CategoriesController(AppDbContext db, IMapper mapper)
+    {
+        this._db = db;
+        this._mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAll()
+    {
+        var categories = await _db.Categories.ToListAsync();
+        return Ok(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+    }
+
+    [HttpGet("{id}/items")]
+    public async Task<ActionResult<IEnumerable<ItemDto>>> GetItemsInCategory(int id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return NotFound();
+        }
+
+        var items = await _db.Items
+            .Include(i => i.Category)
+            .Where(i => i.CategoryId == id)
+            .ToListAsync();
+
+        return Ok(_mapper.Map<IEnumerable<ItemDto>>(items));
+    }
+}
---
+++ WhereToSpendYourTime.Api/Controllers/ItemsController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhereToSpendYourTime.Api.Models.Item;
+using WhereToSpendYourTime.Data;
+
+namespace WhereToSpendYourTime.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ItemsController : ControllerBase
+{
+    private readonly AppDbContext _db;
+    private readonly IMapper _mapper;
+
+    public ItemsController(AppDbContext db, IMapper mapper)
+    {
+        this._db = db;
+        this._mapper = mapper;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ItemDto>> GetItemById(int id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return NotFound();
+        }
+
+        var item = await _db.Items
+            .Include(i => i.Category)
+            .FirstOrDefaultAsync(i => i.Id == id);
+
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(_mapper.Map<ItemDto>(item));
+    }
+}
