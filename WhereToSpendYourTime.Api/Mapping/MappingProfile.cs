21deeaa823f7f53849b8d597ba842f1f3b237c53
Yehor Titov <tegor901@gmail.com>
2025-06-21 14:37:13 +0300

    - Add mapping from entity classes to Dtos

---
+++ WhereToSpendYourTime.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WhereToSpendYourTime.Api.Models.Category;
+using WhereToSpendYourTime.Api.Models.Item;
+using WhereToSpendYourTime.Data.Entities;
+
+namespace WhereToSpendYourTime.Api.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Category, CategoryDto>();
+        CreateMap<Item, ItemDto>()
+            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+    }
+}
