0d5fec9f44271a3206170a650a4df85e2fe22166
Yehor Titov <tegor901@gmail.com>
2025-06-23 15:03:37 +0300

    - Add Dto class for ApplicationUser - Add mapping from ApplicationUser to Dto class

--- WhereToSpendYourTime.Api/Mapping/MappingProfile.cs
+++ WhereToSpendYourTime.Api/Mapping/MappingProfile.cs
@@ -3,6 +3,7 @@ using WhereToSpendYourTime.Api.Models.Category;
 using WhereToSpendYourTime.Api.Models.Comment;
 using WhereToSpendYourTime.Api.Models.Item;
 using WhereToSpendYourTime.Api.Models.Review;
+using WhereToSpendYourTime.Api.Models.User;
 using WhereToSpendYourTime.Data.Entities;
 
 namespace WhereToSpendYourTime.Api.Mapping;
@@ -18,5 +19,6 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.User!.DisplayName));
         CreateMap<Comment, CommentDto>()
             .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.User!.DisplayName));
+        CreateMap<ApplicationUser, ApplicationUserDto>();
     }
 }
---
+++ WhereToSpendYourTime.Api/Models/User/ApplicationUserDto.cs
@@ -0,0 +1,14 @@
+﻿using WhereToSpendYourTime.Api.Models.Comment;
+using WhereToSpendYourTime.Api.Models.Review;
+
+namespace WhereToSpendYourTime.Api.Models.User;
+
+public class ApplicationUserDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public string? Email { get; set; }
+
+    public List<ReviewDto> Reviews { get; set; } = new List<ReviewDto>();
+    public List<CommentDto> Comments { get; set; } = new List<CommentDto>();
+}
