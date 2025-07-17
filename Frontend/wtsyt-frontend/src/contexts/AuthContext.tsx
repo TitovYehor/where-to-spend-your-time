60b7b0556e7365acafebd162e6b5e299964cef63
Yehor Titov <tegor901@gmail.com>
2025-07-17 13:28:37 +0300

    - Add loading constant to context for indicating whether user info was loaded

--- Frontend/wtsyt-frontend/src/contexts/AuthContext.tsx
+++ Frontend/wtsyt-frontend/src/contexts/AuthContext.tsx
@@ -2,9 +2,12 @@ import { createContext, useContext, useEffect, useState } from "react";
 import type { AuthUser } from "../types/authUser";
 import { getMyProfile } from "../services/userService";
 import { logout } from "../services/authService";
+import { handleApiError } from "../utils/handleApi";
+import { useNavigate } from "react-router-dom";
 
 type AuthContextType = {
   user: AuthUser | null;
+  loading: boolean;
   setUser: (user: AuthUser | null) => void;
   userLogout: () => void;
   refreshUser: () => void;
@@ -14,12 +17,20 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<AuthUser | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUser = async () => {
       await getMyProfile()
-        .then(setUser)
-        .catch(() => setUser(null))
+        .then((u) => {
+          setUser(u);
+        })
+        .catch(() => {
+          setUser(null);
+        })
+        .finally(() => setLoading(false));
     };
 
     fetchUser();
@@ -28,18 +39,27 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   const refreshUser = async () => {
     await getMyProfile()
-      .then(setUser)
-      .catch(() => setUser(null))
+      .then((u) => {
+        setUser(u);
+      })
+      .catch(() => {
+        setUser(null);
+      })
+      .finally(() => setLoading(false));
   };
 
   const userLogout = async () => {
     await logout()
-      .catch((e) => console.error("Logout failed", e))
-      .finally(() => setUser(null))
+      .catch(handleApiError)
+      .finally(() => {
+        setUser(null);
+        setLoading(false);
+        navigate("/");
+      })
   };
 
   return (
-    <AuthContext.Provider value={{ user, setUser, userLogout, refreshUser }}>
+    <AuthContext.Provider value={{ user, loading, setUser, userLogout, refreshUser }}>
       {children}
     </AuthContext.Provider>
   );
