c063cb82828517f177dd1230f0ee2c5551e2368c
Yehor Titov <tegor901@gmail.com>
2025-07-18 11:20:23 +0300

    - Add routing for admin pages in App

--- Frontend/wtsyt-frontend/src/App.tsx
+++ Frontend/wtsyt-frontend/src/App.tsx
@@ -14,18 +14,23 @@ import Stats from './pages/Stats.tsx';
 import Header from './components/Header.tsx';
 import RequireAdmin from './components/RequireAdmin.tsx';
 import AdminDashboard from './pages/admin/AdminDashboard.tsx';
+import AdminCategories from './pages/admin/AdminCategories.tsx';
+import AdminItems from './pages/admin/AdminItems.tsx';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800">
       <Header />
       <Routes>
-        <Route path="/admin" element={ <RequireAdmin><AdminDashboard/></RequireAdmin> } />
         <Route path="/login" element={ <Login/> } />
         <Route path="/register" element={ <Register/> } />
         <Route path="/" element={ <Home/> } />
         
         <Route element={ <Layout/> }>
+          <Route path="/admin" element={ <RequireAdmin><AdminDashboard/></RequireAdmin> } />
+          <Route path="/admin/categories" element={ <RequireAdmin><AdminCategories/></RequireAdmin> } />
+          <Route path="/admin/items" element={ <RequireAdmin><AdminItems/></RequireAdmin> } />
+
           <Route path="/profile" element={ <Profile/> } />
           <Route path="/users/:userId" element={ <PublicProfile/> } />
           <Route path="/items" element={ <Items/> } />
