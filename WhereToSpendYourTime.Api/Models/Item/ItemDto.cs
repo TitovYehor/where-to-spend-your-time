babb4964c7921d48d1e3fd614ba066a9dbf932df
Yehor Titov <tegor901@gmail.com>
2025-07-18 11:16:07 +0300

    - Add CategoryId property for ItemDto

--- WhereToSpendYourTime.Api/Models/Item/ItemDto.cs
+++ WhereToSpendYourTime.Api/Models/Item/ItemDto.cs
@@ -7,7 +7,9 @@ public class ItemDto
     public string Title { get; set; } = string.Empty;
     
     public string Description { get; set; } = string.Empty;
-    
+
+    public int CategoryId { get; set; }
+
     public string CategoryName { get; set; } = string.Empty;
 
     public double AverageRating { get; set; }
