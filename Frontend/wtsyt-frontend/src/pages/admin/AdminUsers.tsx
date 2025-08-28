a4f8ee005ddc3db7a29f7de015bf180d3ab9d6b5
Yehor Titov <tegor901@gmail.com>
2025-08-28 09:55:58 +0300

    - Minor fix to search field height

--- Frontend/wtsyt-frontend/src/pages/admin/AdminUsers.tsx
+++ Frontend/wtsyt-frontend/src/pages/admin/AdminUsers.tsx
@@ -201,7 +201,7 @@ export default function AdminUsers() {
             placeholder="Search users..."
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            className="w-full border border-gray-300 px-4 py-2 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full border border-gray-300 px-4 py-1.5 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
 
