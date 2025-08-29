4b76c92703ea8121171c070431478f60b57d5b3e
Yehor Titov <tegor901@gmail.com>
2025-08-29 10:39:12 +0300

    - Add MediaController

---
+++ WhereToSpendYourTime.Api/Controllers/MediaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhereToSpendYourTime.Api.Models.Media;
+using WhereToSpendYourTime.Api.Services.Media;
+
+namespace WhereToSpendYourTime.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MediaController : ControllerBase
+{
+    private readonly IMediaService _mediaService;
+
+    public MediaController(IMediaService mediaService)
+    {
+        this._mediaService = mediaService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadMedia([FromForm] CreateMediaDto dto)
+    {
+        var media = await _mediaService.UploadAsync(dto);
+        return Ok(media);
+    }
+}
