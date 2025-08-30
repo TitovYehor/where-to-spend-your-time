934eb86dc58728325424b20bc9d7911785335ee6
Yehor Titov <tegor901@gmail.com>
2025-08-30 11:29:29 +0300

    - Add media service

---
+++ Frontend/wtsyt-frontend/src/services/mediaService.ts
@@ -0,0 +1,19 @@
+import api from '../api/axios.ts';
+import type { Media, MediaUploadRequest } from '../types/media.ts';
+
+export const uploadMedia = async (request: MediaUploadRequest): Promise<Media> => {
+  const formData = new FormData();
+  formData.append('itemId', request.itemId.toString());
+  formData.append('type', request.type);
+  formData.append('file', request.file);
+
+  const res = await api.post<Media>('/media', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+
+  return res.data;
+};
+
+export const getMediaUrl = (relativeUrl: string) => {
+  return `https://localhost:7005${relativeUrl}`;
+};
