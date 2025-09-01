8c901604784ad3958bc4f9920eaf4e8724ac36c6
Yehor Titov <tegor901@gmail.com>
2025-09-01 13:59:43 +0300

    - Modify profile pages styling (make them less transparent, more contrast)

--- Frontend/wtsyt-frontend/src/pages/Profile.tsx
+++ Frontend/wtsyt-frontend/src/pages/Profile.tsx
@@ -102,7 +102,7 @@ const Profile = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-white/60 backdrop-blur-md rounded-xl shadow-lg">
+    <div className="max-w-4xl mx-auto p-8 bg-white/80 backdrop-blur-md rounded-2xl shadow-xl">
       <article className="mb-6">
         <h1 className="text-3xl font-bold mb-2">Your Profile</h1>
         <p><strong>Name:</strong> {user?.displayName}</p>
--- Frontend/wtsyt-frontend/src/pages/PublicProfilePage.tsx
+++ Frontend/wtsyt-frontend/src/pages/PublicProfilePage.tsx
@@ -38,7 +38,7 @@ export default function PublicProfile() {
   }
 
   return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-white/60 backdrop-blur-md rounded-xl shadow-lg">
+    <div className="max-w-4xl mx-auto p-8 bg-white/80 backdrop-blur-md rounded-2xl shadow-xl">
       <article className="mb-10">
         <h1 className="text-3xl font-bold mb-2">{user?.displayName}'s Profile</h1>
         <p><strong>Reviews count:</strong> {user?.reviews.length}</p>
