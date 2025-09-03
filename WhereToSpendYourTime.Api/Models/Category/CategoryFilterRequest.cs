b4a4d7bf4badd4f2967f932966b10f5807a595dd
Yehor Titov <tegor901@gmail.com>
2025-09-03 11:01:46 +0300

    - Add CategoryFilterRequest class for transfering data filters (page, pageSize, search) - Add PagedCategoryResult for categories with pagination

---
+++ WhereToSpendYourTime.Api/Models/Category/CategoryFilterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WhereToSpendYourTime.Api.Models.Category;
+
+public class CategoryFilterRequest
+{
+    public int Page { get; set; } = 1;
+
+    public int PageSize { get; set; } = 5;
+
+    public string? Search { get; set; } = string.Empty;
+}
---
+++ WhereToSpendYourTime.Api/Models/Category/PagedCategoryResult.cs
@@ -0,0 +1,8 @@
+﻿namespace WhereToSpendYourTime.Api.Models.Category;
+
+public class PagedCategoryResult
+{
+    public List<CategoryDto> Categories { get; set; } = [];
+
+    public int TotalCount { get; set; }
+}
