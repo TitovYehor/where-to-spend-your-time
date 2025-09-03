7ae17761da228d04a3cc0df2cdd36b992c7144f1
Yehor Titov <tegor901@gmail.com>
2025-09-03 11:32:16 +0300

    - Add generic paged types for items/categories/tags

---
+++ Frontend/wtsyt-frontend/src/types/pagination/pagedResult.ts
@@ -0,0 +1,12 @@
+import type { Category } from "../category";
+import type { Item } from "../item";
+import type { Tag } from "../tag";
+
+export interface PagedResult<T> {
+    items: T[];
+    totalCount: number;
+};
+
+export type ItemPagedResult = PagedResult<Item>;
+export type CategoryPagedResult = PagedResult<Category>;
+export type TagPagedResult = PagedResult<Tag>;
