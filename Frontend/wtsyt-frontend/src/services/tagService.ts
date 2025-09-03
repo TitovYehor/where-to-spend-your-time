7be661ec3ed0f1f66216ed4a6a5bf0a92ca04c47
Yehor Titov <tegor901@gmail.com>
2025-09-03 12:42:56 +0300

    - Add build tag query and getPagedTags function for tag service

--- Frontend/wtsyt-frontend/src/services/tagService.ts
+++ Frontend/wtsyt-frontend/src/services/tagService.ts
@@ -1,8 +1,33 @@
 import api from '../api/axios.ts';
+import type { TagPagedResult } from '../types/pagination/pagedResult.ts';
 import type { Tag, TagCreateRequest, TagUpdateRequest } from '../types/tag.ts';
+
+export const buildTagQuery = (params: {
+  search?: string;
+  page?: number;
+  pageSize?: number;
+}): string => {
+  const query = new URLSearchParams();
+
+  if (params.search) query.append("search", params.search);
+  if (params.page !== undefined) query.append("page", params.page.toString());
+  if (params.pageSize !== undefined) query.append("pageSize", params.pageSize.toString());
+  
+  return query.toString();
+};
 
 export const getTags = async (): Promise<Tag[]> => {
   const res = await api.get<Tag[]>('/tags');
+  return res.data;
+};
+
+export const getPagedTags = async (params: {
+  search?: string;
+  page?: number;
+  pageSize?: number;
+}): Promise<TagPagedResult> => {
+  const query = buildTagQuery(params);
+  const res = await api.get<TagPagedResult>(`/tags/paged?${query}`);
   return res.data;
 };
 
