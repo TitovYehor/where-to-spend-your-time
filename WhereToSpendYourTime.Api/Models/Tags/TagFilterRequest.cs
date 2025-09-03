0abf45356de715ffab2ea14584ede06ae902dfe2
Yehor Titov <tegor901@gmail.com>
2025-09-03 12:41:07 +0300

    - Add filter request for tags

---
+++ WhereToSpendYourTime.Api/Models/Tags/TagFilterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WhereToSpendYourTime.Api.Models.Tags;
+
+public class TagFilterRequest
+{
+    public int Page { get; set; } = 1;
+
+    public int PageSize { get; set; } = 5;
+
+    public string? Search { get; set; } = string.Empty;
+}
