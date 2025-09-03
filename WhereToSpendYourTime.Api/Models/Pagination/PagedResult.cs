fd204aa8041caa589da4e3ac86070c6a0b9ccd26
Yehor Titov <tegor901@gmail.com>
2025-09-03 10:59:38 +0300

    - Add generic PagedResult class to use it for all classes which implement pagination - Add extension to get paged result from queries

---
+++ WhereToSpendYourTime.Api/Models/Pagination/PagedResult.cs
@@ -0,0 +1,14 @@
+﻿namespace WhereToSpendYourTime.Api.Models.Pagination;
+
+public class PagedResult<T>
+{
+    public List<T> Items { get; }
+
+    public int TotalCount { get; }
+
+    public PagedResult(List<T> items, int totalCount)
+    {
+        this.Items = items;
+        this.TotalCount = totalCount;
+    }
+}
