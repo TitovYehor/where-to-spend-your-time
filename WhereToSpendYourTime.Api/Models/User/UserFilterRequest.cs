d5dcfd5926a21860238e9bf18bd11669ecffceba
Yehor Titov <tegor901@gmail.com>
2025-09-04 17:28:46 +0300

    - Add UserFilterRequest class for pagination

---
+++ WhereToSpendYourTime.Api/Models/User/UserFilterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WhereToSpendYourTime.Api.Models.User;
+
+public class UserFilterRequest
+{
+    public int Page { get; set; } = 1;
+
+    public int PageSize { get; set; } = 5;
+
+    public string? Search { get; set; } = string.Empty;
+}
