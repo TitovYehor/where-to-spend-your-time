fc854d56c8d6d50bb2e738f31de1233a2647342e
Yehor Titov <tegor901@gmail.com>
2025-09-12 10:29:41 +0200

    - Add endpoints for each backend controller

---
+++ Frontend/wtsyt-frontend/src/api/endpoints.ts
@@ -0,0 +1,11 @@
+export const API_BASES = {
+  auth: "/auth",
+  categories: "/categories",
+  reviews: "/reviews",
+  users: "/users",
+  comments: "/comments",
+  items: "/items",
+  tags: "/tags",
+  stats: "/stats",
+  media: "/media"
+};
