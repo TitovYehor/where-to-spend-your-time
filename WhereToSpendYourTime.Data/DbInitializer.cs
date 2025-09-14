2f6efc8d9de911a83dbebfc297e29a3aef900656
Yehor Titov <tegor901@gmail.com>
2025-09-14 10:54:17 +0200

    - Remove import of unused libraries

--- WhereToSpendYourTime.Data/DbInitializer.cs
+++ WhereToSpendYourTime.Data/DbInitializer.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WhereToSpendYourTime.Data.Entities;
 
