ef67c46fb051534b0b197fcb492c1d81c1c42e5c
Yehor Titov <tegor901@gmail.com>
2025-11-21 18:34:32 +0100

    - Add ApplicationRole and ApplicationUserRole for easier retrieving process of users roles

---
+++ WhereToSpendYourTime.Data/Entities/ApplicationRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WhereToSpendYourTime.Data.Entities;
+
+public class ApplicationRole : IdentityRole
+{
+    public ApplicationRole() : base() { }
+
+    public ApplicationRole(string roleName) : base(roleName)
+    {
+    }
+
+    public ICollection<ApplicationUserRole> UserRoles { get; set; } = new List<ApplicationUserRole>();
+}
---
+++ WhereToSpendYourTime.Data/Entities/ApplicationUserRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WhereToSpendYourTime.Data.Entities;
+
+public class ApplicationUserRole : IdentityUserRole<string>
+{
+    public ApplicationUser User { get; set; } = null!;
+    public ApplicationRole Role { get; set; } = null!;
+}
