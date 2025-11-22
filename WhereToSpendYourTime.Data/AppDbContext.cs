1111a91e220742bd1ffbc175d47724617cd2ece3
Yehor Titov <tegor901@gmail.com>
2025-11-22 10:18:17 +0100

    - Add ApplicationUserRole entity to context

--- WhereToSpendYourTime.Data/AppDbContext.cs
+++ WhereToSpendYourTime.Data/AppDbContext.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WhereToSpendYourTime.Data.Entities;
 
 namespace WhereToSpendYourTime.Data;
 
-public class AppDbContext : IdentityDbContext<ApplicationUser>
+public class AppDbContext
+    : IdentityDbContext<ApplicationUser, ApplicationRole, string,
+        IdentityUserClaim<string>, ApplicationUserRole,
+        IdentityUserLogin<string>, IdentityRoleClaim<string>,
+        IdentityUserToken<string>>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
@@ -14,12 +19,26 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Tag> Tags { get; set; }
     public DbSet<ItemTag> ItemTags { get; set; }
-
     public DbSet<Media> Media { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<ApplicationUserRole>(entity =>
+        {
+            entity.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+            entity.HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+
+            entity.HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+        });
 
         builder.Entity<ApplicationUser>()
             .Property(u => u.DisplayName)
@@ -51,7 +70,7 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
             .WithMany(t => t.ItemTags)
             .HasForeignKey(it => it.TagId)
             .OnDelete(DeleteBehavior.Cascade);
-    
+
         builder.Entity<Media>()
             .HasOne(m => m.Item)
             .WithMany(i => i.Media)
