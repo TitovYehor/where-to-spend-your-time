80b7305832ee5b67e9956ab4eb1c75fd702a4e3a
Yehor Titov <tegor901@gmail.com>
2025-12-24 14:44:04 +0100

    - Add AbortSignal compatibility

--- Frontend/wtsyt-frontend/src/services/statsService.ts
+++ Frontend/wtsyt-frontend/src/services/statsService.ts
@@ -2,7 +2,7 @@ import api from '../api/axios.ts';
 import { API_BASES } from '../api/endpoints.ts';
 import type { Stats } from '../types/stats.ts';
 
-export const getStats = async (): Promise<Stats> => {
-  const res = await api.get<Stats>(`${API_BASES.stats}`);
+export const getStats = async (signal?: AbortSignal): Promise<Stats> => {
+  const res = await api.get<Stats>(`${API_BASES.stats}`, { signal });
   return res.data;
 };
