d768cf64f03fa2b8505a45bb1d155ef6ef293805
Yehor Titov <tegor901@gmail.com>
2026-01-08 10:15:55 +0100

    - Refactor MappingProfile to avoid possible errors and make it more readable

--- WhereToSpendYourTime.Api/Mapping/MappingProfile.cs
+++ WhereToSpendYourTime.Api/Mapping/MappingProfile.cs
@@ -12,35 +12,84 @@ namespace WhereToSpendYourTime.Api.Mapping;
 
 public class MappingProfile : Profile
 {
+    private const string Unknown = "Unknown";
+    private const string DefaultUserRole = "User";
+
     public MappingProfile()
     {
+        ConfigureCategoryMappings();
+        ConfigureTagMappings();
+        ConfigureItemMappings();
+        ConfigureReviewMappings();
+        ConfigureCommentMappings();
+        ConfigureUserMappings();
+        ConfigureMediaMappings();
+    }
+
+    private void ConfigureCategoryMappings()
+    {
         CreateMap<Category, CategoryDto>();
+    }
+
+    private void ConfigureTagMappings()
+    {
         CreateMap<Tag, TagDto>();
+    }
+
+    private void ConfigureItemMappings()
+    {
         CreateMap<Item, ItemDto>()
-            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category != null ? src.Category.Name : "Unknown"))
+            .ForMember(dest => dest.CategoryName,
+                opt => opt.MapFrom(src => src.Category != null ? src.Category.Name : Unknown))
             .ForMember(dest => dest.AverageRating,
                 opt => opt.MapFrom(src => src.Reviews.Select(r => (double?)r.Rating).Average() ?? 0))
             .ForMember(dest => dest.Media,
                 opt => opt.MapFrom(src => src.Media
                     .OrderByDescending(m => m.Type)
                     .ThenBy(m => m.Id)))
-            .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.ItemTags.Select(it => it.Tag)));
+            .ForMember(dest => dest.Tags,
+                opt => opt.MapFrom(src => src.ItemTags.Select(it => it.Tag)));
+    }
+
+    private void ConfigureReviewMappings()
+    {
         CreateMap<Review, ReviewDto>()
-            .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.User!.DisplayName))
+            .ForMember(dest => dest.Author,
+                opt => opt.MapFrom(src => src.User != null ? src.User.DisplayName : Unknown))
             .ForMember(dest => dest.AuthorRole,
                 opt => opt.MapFrom(src =>
-                    src.User!.UserRoles
-                        .Select(ur => ur.Role.Name)
-                        .FirstOrDefault() ?? "User"))
-            .ForMember(dest => dest.ItemTitle, opt => opt.MapFrom(src => src.Item!.Title));
+                    src.User != null
+                        ? src.User.UserRoles
+                            .Select(ur => ur.Role.Name)
+                            .OrderBy(name => name)
+                            .FirstOrDefault() ?? DefaultUserRole
+                        : DefaultUserRole))
+            .ForMember(dest => dest.ItemTitle,
+                opt => opt.MapFrom(src => src.Item != null ? src.Item.Title : Unknown));
+    }
+
+    private void ConfigureCommentMappings()
+    {
         CreateMap<Comment, CommentDto>()
-            .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.User!.DisplayName))
+            .ForMember(dest => dest.Author,
+                opt => opt.MapFrom(src => src.User != null ? src.User.DisplayName : Unknown))
             .ForMember(dest => dest.AuthorRole,
                 opt => opt.MapFrom(src =>
-                    src.User!.UserRoles
-                        .Select(ur => ur.Role.Name)
-                        .FirstOrDefault() ?? "User"));
+                    src.User != null
+                        ? src.User.UserRoles
+                            .Select(ur => ur.Role.Name)
+                            .OrderBy(name => name)
+                            .FirstOrDefault() ?? DefaultUserRole
+                        : DefaultUserRole));
+    }
+
+    private void ConfigureUserMappings()
+    {
         CreateMap<ApplicationUser, ApplicationUserDto>();
+    }
+
+    private void ConfigureMediaMappings()
+    {
         CreateMap<Media, MediaDto>();
     }
 }
