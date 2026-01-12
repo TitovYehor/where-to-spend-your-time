0118b968b9f37f33064f104d0d11df082ccdf6c3
Yehor Titov <tegor901@gmail.com>
2026-01-12 09:44:25 +0100

    - Small refactoring by adding parameters null checks and adding private constant for default UserRole

--- WhereToSpendYourTime.Api/Services/Auth/AuthService.cs
+++ WhereToSpendYourTime.Api/Services/Auth/AuthService.cs
@@ -9,6 +9,8 @@ public class AuthService : IAuthService
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
 
+    private const string DefaultRole = "User";
+
     public AuthService(UserManager<ApplicationUser> userManager,
                        SignInManager<ApplicationUser> signInManager)
     {
@@ -18,6 +20,11 @@ public class AuthService : IAuthService
 
     public async Task<(bool Succeeded, IEnumerable<IdentityError> Errors)> RegisterAsync(RegisterRequest request)
     {
+        if (request == null)
+        {
+            throw new ArgumentNullException(nameof(request));
+        }
+
         var user = new ApplicationUser
         {
             UserName = request.Email,
@@ -31,13 +38,15 @@ public class AuthService : IAuthService
             return (false, result.Errors);
         }
 
-        await _userManager.AddToRoleAsync(user, "User");
+        await _userManager.AddToRoleAsync(user, DefaultRole);
 
-        return (true, Enumerable.Empty<IdentityError>());
+        return (true, Array.Empty<IdentityError>());
     }
 
     public async Task<bool> LoginAsync(LoginRequest request)
     {
+        ArgumentNullException.ThrowIfNull(request);
+
         var user = await _userManager.FindByEmailAsync(request.Email);
         if (user == null)
         {
@@ -45,11 +54,12 @@ public class AuthService : IAuthService
         }
 
         var result = await _signInManager.PasswordSignInAsync(user, request.Password, isPersistent: true, lockoutOnFailure: false);
+
         return result.Succeeded;
     }
 
     public async Task LogoutAsync()
-    { 
+    {
         await _signInManager.SignOutAsync();
     }
 }
