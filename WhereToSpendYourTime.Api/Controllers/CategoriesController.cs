bfd10295bed6738923d5cb20433a062a7b483da3
Yehor Titov <tegor901@gmail.com>
2026-01-26 09:56:20 +0100

    - Modify controller methods to make it more RESTful by removing all business logic (such as returning BadRequests) following changes in CategoryService

--- WhereToSpendYourTime.Api/Controllers/CategoriesController.cs
+++ WhereToSpendYourTime.Api/Controllers/CategoriesController.cs
@@ -17,62 +17,46 @@ public class CategoriesController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAll()
+    public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAll()
     {
         var categories = await _categoryService.GetAllCategoriesAsync();
-
         return Ok(categories);
     }
 
     [HttpGet("paged")]
-    public async Task<IActionResult> GetPagedCategories([FromQuery] CategoryFilterRequest filter)
-    { 
+    public async Task<ActionResult<Models.Pagination.PagedResult<CategoryDto>>> GetPagedCategories([FromQuery] CategoryFilterRequest filter)
+    {
         var categories = await _categoryService.GetPagedCategoriesAsync(filter);
-
         return Ok(categories);
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetCategoryById(int id)
+    public async Task<ActionResult<CategoryDto>> GetCategoryById(int id)
     {
         var category = await _categoryService.GetByIdAsync(id);
-        return category == null ? NotFound() : Ok(category);
+        return Ok(category);
     }
 
     [HttpGet("{id}/items")]
-    public async Task<IActionResult> GetItemsInCategory(int id)
+    public async Task<ActionResult<IEnumerable<Models.Item.ItemDto>>> GetItemsInCategory(int id)
     {
-        if (!ModelState.IsValid)
-        {
-            return NotFound();
-        }
-
         var items = await _categoryService.GetItemsByCategoryIdAsync(id);
-
         return Ok(items);
     }
 
     [Authorize(Roles = "Admin")]
     [HttpPost]
-    public async Task<IActionResult> CreateCategory(CategoryCreateRequest request)
+    public async Task<IActionResult> CreateCategory([FromBody] CategoryCreateRequest request)
     {
         var category = await _categoryService.CreateCategoryAsync(request);
-
-        return category == null
-            ? BadRequest("Error during category creation") 
-            : CreatedAtAction(nameof(GetCategoryById), new { id = category.Id }, category);
+        return CreatedAtAction(nameof(GetCategoryById), new { id = category.Id }, category);
     }
 
     [Authorize(Roles = "Admin")]
     [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateCategory(int id, CategoryUpdateRequest request)
+    public async Task<IActionResult> UpdateCategory(int id, [FromBody] CategoryUpdateRequest request)
     {
-        var result = await _categoryService.UpdateCategoryAsync(id, request);
-        if (!result)
-        {
-            return NotFound();
-        }
-
+        await _categoryService.UpdateCategoryAsync(id, request);
         return NoContent();
     }
 
@@ -80,12 +64,7 @@ public class CategoriesController : ControllerBase
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteCategory(int id)
     {
-        var result = await _categoryService.DeleteCategoryAsync(id);
-        if (!result)
-        {
-            return NotFound();
-        }
-
+        await _categoryService.DeleteCategoryAsync(id);
         return NoContent();
     }
 }
