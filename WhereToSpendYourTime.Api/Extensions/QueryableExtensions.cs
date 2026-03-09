4b70d6d67534118c489fd1d4bb571094c07d27da
Yehor Titov <tegor901@gmail.com>
2026-03-09 07:49:07 +0100

    - Add documentation for the QueryableExtensions extension

--- WhereToSpendYourTime.Api/Extensions/QueryableExtensions.cs
+++ WhereToSpendYourTime.Api/Extensions/QueryableExtensions.cs
@@ -3,10 +3,37 @@ using WhereToSpendYourTime.Api.Models.Pagination;
 
 namespace WhereToSpendYourTime.Api.Extensions;
 
+/// <summary>
+/// Provides extension methods for IQueryable to apply
+/// common query operations such as pagination
+/// </summary>
 public static class QueryableExtensions
 {
+    /// <summary>
+    /// Converts the specified query into a paged result
+    /// </summary>
+    /// <remarks>
+    /// Executes two database queries:
+    /// one to retrieve the total number of records and another
+    /// to fetch the requested page of data.
+    /// Page numbering starts from 1.
+    /// The query should already contain filtering and ordering
+    /// before calling this method
+    /// </remarks>
+    /// <param name="query">
+    /// The source query to paginate
+    /// </param>
+    /// <param name="page">
+    /// Page number starting from 1
+    /// </param>
+    /// <param name="pageSize">
+    /// Number of items per page
+    /// </param>
+    /// <returns>
+    /// A PagedResult containing paginated items and total count
+    /// </returns>
     public static async Task<PagedResult<T>> ToPagedResultAsync<T>(
-        this IQueryable<T> query, int page, int pageSize) 
+        this IQueryable<T> query, int page, int pageSize)
     {
         var totalCount = await query.CountAsync();
 
