20ab4c8c1c43ce59aefb9db3fd96f31293038b9d
Yehor Titov <tegor901@gmail.com>
2026-03-13 07:41:23 +0100

    - Add documentation for the Media models

--- WhereToSpendYourTime.Api/Models/Media/CreateMediaDto.cs
+++ WhereToSpendYourTime.Api/Models/Media/CreateMediaDto.cs
@@ -3,14 +3,26 @@ using WhereToSpendYourTime.ShareLib.Enums;
 
 namespace WhereToSpendYourTime.Api.Models.Media;
 
+/// <summary>
+/// Represents a request to upload media for an item
+/// </summary>
 public class CreateMediaDto
 {
+    /// <summary>
+    /// The identifier of the item the media belongs to
+    /// </summary>
     [Required]
     public int ItemId { get; set; }
 
+    /// <summary>
+    /// The type of media being uploaded
+    /// </summary>
     [Required]
     public MediaType Type { get; set; }
 
+    /// <summary>
+    /// The media file to upload
+    /// </summary>
     [Required]
     public IFormFile File { get; set; } = null!;
 }
--- WhereToSpendYourTime.Api/Models/Media/MediaDto.cs
+++ WhereToSpendYourTime.Api/Models/Media/MediaDto.cs
@@ -2,11 +2,23 @@
 
 namespace WhereToSpendYourTime.Api.Models.Media;
 
+/// <summary>
+/// Data transfer object representing media associated with an item
+/// </summary>
 public class MediaDto
 {
+    /// <summary>
+    /// The unique identifier of the media
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// The type of the media
+    /// </summary>
     public MediaType Type { get; set; }
-    
+
+    /// <summary>
+    /// The URL where the media file is stored
+    /// </summary>
     public string Url { get; set; } = string.Empty;
 }
