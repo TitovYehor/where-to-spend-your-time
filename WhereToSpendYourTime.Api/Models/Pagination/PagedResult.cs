72f8f7b8f9a4c3d19ee14faba665e1aa793bef30
Yehor Titov <tegor901@gmail.com>
2026-03-13 07:42:12 +0100

    - Add documentation for the PagedResult model

--- WhereToSpendYourTime.Api/Models/Pagination/PagedResult.cs
+++ WhereToSpendYourTime.Api/Models/Pagination/PagedResult.cs
@@ -1,9 +1,18 @@
 ﻿namespace WhereToSpendYourTime.Api.Models.Pagination;
 
+/// <summary>
+/// Represents a paginated result returned from a query
+/// </summary>
 public class PagedResult<T>
 {
+    /// <summary>
+    /// The list of items for the current page
+    /// </summary>
     public List<T> Items { get; }
 
+    /// <summary>
+    /// The total number of items available before pagination is applied
+    /// </summary>
     public int TotalCount { get; }
 
     public PagedResult(List<T> items, int totalCount)
