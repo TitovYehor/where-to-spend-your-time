bca5ee8227f8f2718ac3c1fb153f6de7579ead0b
Yehor Titov <tegor901@gmail.com>
2026-03-31 07:33:02 +0200

    - Add EmailSettings to get data from program configuration

---
+++ WhereToSpendYourTime.Api/Services/Email/Config/EmailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace WhereToSpendYourTime.Api.Services.Email.Config;
+
+public class EmailSettings
+{
+    public string SmtpHost { get; set; } = string.Empty;
+    public int SmtpPort { get; set; }
+    public bool EnableSsl { get; set; }
+    public string SenderEmail { get; set; } = string.Empty;
+    public string SenderName { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
